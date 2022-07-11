7472488ac4a81e4d33fba302aacbf8ee3a82b03d
Alex <otostogan@yandex.ru>
2022-07-11 12:41:10 +0300

    Динамические параметры

--- express/routes/courses.js
+++ express/routes/courses.js
@@ -14,4 +14,16 @@ router.get('/', async (req, res) => {
     })
 })
 
+router.get('/:id', async (req, res) => {
+    
+    const course = await Course.getById(req.params.id);
+
+    res.status(200);
+    res.render('course', {
+        layout: 'empty',
+        title: course.title,
+        course
+    })
+})
+
 module.exports = router;
