7542a664ddb4160bc95c5ea6808739efca6bda78
Alex <otostogan@yandex.ru>
2022-07-11 11:15:33 +0300

    Обработка формы

--- express/index.js
+++ express/index.js
@@ -19,6 +19,8 @@ app.set('views', 'views');
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(express.urlencoded({extended: true}));
+
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
